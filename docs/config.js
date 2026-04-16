29592b2e0dd2064f4c0f42347919bbd62ca91a77
hitrustt <116234449+hitrustt@users.noreply.github.com>
2026-04-15 22:23:09 -0500

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   SUPABASE_URL:      'https://ruabhtjybgeqvtnffvlm.supabase.co',       // e.g. https://xyzxyz.supabase.co
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1YWJodGp5YmdlcXZ0bmZmdmxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMTM0MTIsImV4cCI6MjA5MTc4OTQxMn0.fpQyFqnF2ohQmGU0ul1TNfE-VvE1kJey9PU_f810C3w',  // starts with eyJ...
 
-  LIMIT: 100,
+  LIMIT: 50,
 
   GAME_NAME: 'LBS',
 
